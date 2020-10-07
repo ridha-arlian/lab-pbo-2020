2f88f20c947b75485ce78d6f792d9f36c00cc6d7
Ridha Arlian Alhaqie <ridha.arlian19@gmail.com>
2020-10-07 21:33:30 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 1/prak2Lat5.java
@@ -0,0 +1,17 @@
+public class prak2Lat5{
+
+	public static void main(String args[]){
+	 
+		boolean a = true;
+		boolean b = false;
+
+		System.out.println("a && b = " + (a&&b)); 
+		System.out.println("a || b = " + (a||b) );
+		System.out.println("!(a && b) = " + !(a && b));
+	}	
+}
+
+/*
+Kode di atas digunakan untuk mencoba operator logika,
+operator ini digunakan untuk pembanding tipe data boolean yang hasilnya true/false.
+*/
---
+++ Pertemuan 1/prak2Lat7.java
@@ -0,0 +1,16 @@
+public class prak2Lat7 {
+
+	public static void main(String[] args) {
+		
+		String name = "James";
+
+		boolean result = name instanceof String; 
+		System.out.println( result );
+	}
+}
+
+/*
+Kode di atas digunakan untuk mencoba operator Instanceof,
+operator ini digunakan untuk membandingkan suatu objek apakah objek tersebut termasuk dari suatu class atau tidak,
+dan dari perbandingan tersebut akan menghasilkan nilai boolean berupa nilai true/false.
+*/
