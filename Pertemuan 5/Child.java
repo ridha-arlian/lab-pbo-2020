2aa4fed2f212c03b2c51435924d2d424780255bd
Ridha Arlian Alhaqie <66559097+ridha-arlian@users.noreply.github.com>
2021-01-02 13:40:36 +0700

    Membuat Kode Pertemuan 5

---
+++ Pertemuan 5/Child.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Child here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Child extends Parent {
+    int x;
+    
+    /* Method constructor yang digunakan untuk menciptakan suatu objek */
+    public Child() 
+    {
+        super();
+        x = 5;
+    }
+}
