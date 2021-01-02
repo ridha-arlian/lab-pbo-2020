b296d6c9bb2087525b81209529afec1347a36343
Ridha Arlian Alhaqie <66559097+ridha-arlian@users.noreply.github.com>
2021-01-02 11:41:45 +0700

    Membuat Kode Pertemuan 4

---
+++ Pertemuan 4/Siswa.java
@@ -0,0 +1,45 @@
+
+/**
+ * Class ini dibuat untuk menyimpan data, kemudian class ini akan dipanggil
+ * pada class TesSiswa
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Siswa
+{
+     private int nrp;
+     private String nama;
+    
+     public Siswa()
+     {
+         this.nrp = 12345;
+         this.nama = "nama default";
+     }
+    
+     public Siswa(int nrp, String nama)
+     {
+         this.nrp = nrp;
+         this.nama = nama;
+     }
+    
+     public void setNrp(int nrp)
+     {
+         this.nrp = nrp;
+     }
+    
+     public int getNrp()
+     {
+         return this.nrp;
+     }
+    
+     public void setNama(String nama)
+     {
+         this.nama = nama;
+     }
+    
+     public String getNama()
+     {
+         return this.nama;
+     }
+}
